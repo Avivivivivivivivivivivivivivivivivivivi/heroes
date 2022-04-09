e54b518575a49d0af02095e342c99ec37f77e00f
Sebastian Szeszko <sebastian.szeszko@primigenius.pl>
2022-04-09 12:22:26 +0200

    add basic router

--- heroes-frontend/src/App.js
+++ heroes-frontend/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg'
 import './App.css'
+import { Link, Outlet, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="hero" element={<HeroListContainer />} />
+          <Route path="hero/:id" element={<HeroContainer />} />
+        </Route>
+      </Routes>
     </div>
+  )
+}
+
+const Layout = () => {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/hero">HeroList</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  )
+}
+
+const HeroContainer = () => {
+  return (
+    <>
+      <h1>Hero page</h1>
+    </>
+  )
+}
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  )
+}
+
+const HeroListContainer = () => {
+  return (
+    <>
+      <h1>Hero list page</h1>
+    </>
   )
 }
 
